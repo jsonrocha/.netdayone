7ff45c9b787d138a1d10f86ae33a20191b3255e7
Rocha <jrocha@proforma.com>
2018-09-10 21:20:23 -0400

    ExplorerMode

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace dotnetdayone
+{
+    class Program
+    {
+        static void Prompt()
+        {
+            System.Console.WriteLine($"Choose a number from 1 to 100");
+        }
+
+        static int Pick()
+        {
+            var userInput = int.Parse(Console.ReadLine());
+            System.Console.WriteLine($"Your choice is {userInput}");
+            return userInput;
+        }
+
+        static int CGuess()
+        {
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1, 100);
+            System.Console.WriteLine($"My guess was going to be {randomNumber}");
+            return randomNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            Prompt();
+            Pick();
+            CGuess();
+        }
+    }
+}
